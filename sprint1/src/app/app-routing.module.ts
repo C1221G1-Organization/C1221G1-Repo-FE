3223f5b31c35d7ea04623f7296cf773a8ff185ec
Nguyen <nguyen2697s@gmail.com>
2022-07-01 16:21:21 +0700

    NghiaNTT update library

--- sprint1/src/app/app-routing.module.ts
+++ sprint1/src/app/app-routing.module.ts
@@ -1,79 +1,85 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 
 const routes: Routes = [
   {
-    path: '',pathMatch: 'full', redirectTo: 'templates'
+    path: '', pathMatch: 'full', redirectTo: 'templates'
   },
   {
-    path: 'customer',
+    path        : 'customer',
     loadChildren: () => import('./customer/customer.module').then(module => module.CustomerModule)
   },
   {
-    path: 'employee',
+    path        : 'employee',
     loadChildren: () => import('./employee/employee.module').then(module => module.EmployeeModule)
   },
   {
-    path: 'account',
+    path        : 'account',
     loadChildren: () => import('./account/account.module').then(module => module.AccountModule)
   },
   {
-    path: 'admin-chat',
+    path        : 'admin-chat',
     loadChildren: () => import('./admin-chat/admin-chat.module').then(module => module.AdminChatModule)
   },
   {
-    path: 'log',
+    path        : 'log',
     loadChildren: () => import('./log/log.module').then(module => module.LogModule)
   },
   {
-    path: 'medicine',
+    path        : 'medicine',
     loadChildren: () => import('./medicine/medicine.module').then(module => module.MedicineModule)
   },
   {
-    path: 'look-up',
+    path        : 'look-up',
     loadChildren: () => import('./look-up/look-up.module').then(module => module.LookUpModule)
   },
   {
-    path: 'cart',
+    path        : 'cart',
     loadChildren: () => import('./cart/cart.module').then(module => module.CartModule)
   },
   {
-    path: 'prescription',
+    path        : 'prescription',
     loadChildren: () => import('./prescription/prescription.module').then(module => module.PrescriptionModule)
   },
   {
-    path: 'reports',
+    path        : 'reports',
     loadChildren: () => import('./reports/report.module').then(module => module.ReportModule)
   },
   {
-    path: 'sales-management',
+    path        : 'sales-management',
     loadChildren: () => import('./sales-management/sales-management.module').then(module => module.SalesManagementModule)
   },
   {
-    path: 'supplier',
+    path        : 'supplier',
     loadChildren: () => import('./supplier/supplier.module').then(module => module.SupplierModule)
   },
   {
-    path: 'templates',
+    path        : 'templates',
     loadChildren: () => import('./templates/templates.module').then(module => module.TemplatesModule)
   },
   {
-    path: 'payment',
+    path        : 'payment',
     loadChildren: () => import('./payment/payment.module').then(module => module.PaymentModule)
   },
   {
-    path: 'import-invoice-medicine',
+    path        : 'import-invoice-medicine',
     loadChildren: () => import('./import-invoice-medicine/import-invoice-medicine.module').then(module => module.ImportInvoiceMedicineModule)
   },
   {
-    path: 'import-invoice',
+    path        : 'import-invoice',
     loadChildren: () => import('./import-invoice/import-invoice.module').then(module => module.ImportInvoiceModule)
   },
+  {
+    path     : '**',
+    component: NotFoundComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- sprint1/src/app/app.module.ts
+++ sprint1/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
