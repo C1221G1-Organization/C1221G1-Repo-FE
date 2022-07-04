743419689311181fe5cbc49389302e41f2f9f73a
QuangTruong224 <changtraitre2000@gmail.com>
2022-07-04 17:15:53 +0700

    TruongNQ update code FE

--- sprint1/src/app/service/customer.service.ts
+++ sprint1/src/app/service/customer.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Customer} from "../model/customer";
 import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 const API_URL = `${environment.apiUrl}`;
 
@@ -21,11 +22,29 @@ export class CustomerService {
 
   // @ts-ignore
   findById(customerId: string): Observable<Customer | any> {
-    return this.http.get(`${API_URL}/api/manager-customer/customers/customerId=${customerId}`);
+    return this.http.get(`${API_URL}/api/manager-customer/customers/${customerId}`);
   }
 
   update(customerId: string, customer: Customer): Observable<Customer | any> {
     return this.http.patch(`${API_URL}/api/manager-customer/customers/${customerId}`, customer);
+  }
+
+
+  checkPhoneNotTaken(customerPhone: string): Observable<boolean> {
+    console.log(customerPhone + "!aemewdsjhgdkjghvsdkjyhg")
+    return this.http.get('http://localhost:8080/api/manager-customer/customers?size=10000').pipe(
+      map((res => {
+            const customerList = res['content']
+            return customerList.filter(prd =>
+
+              prd.customerPhone == customerPhone)
+          }
+        )
+      ), map(customerList =>
+        !customerList.length  // length = 1 => false, length = 0 => true;
+      ))
+
+      ;
   }
 }
 
