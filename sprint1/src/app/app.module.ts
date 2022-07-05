41537020a7d8ae470babd1639f78597b7a10b665
QuangTruong224 <changtraitre2000@gmail.com>
2022-07-05 17:18:10 +0700

    TruongNQ update code FE

--- sprint1/src/app/app.module.ts
+++ sprint1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ToastrModule} from "ngx-toastr";
+import { ComponentComponent } from './component/component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ComponentComponent,
   ],
   imports: [
     BrowserModule,
