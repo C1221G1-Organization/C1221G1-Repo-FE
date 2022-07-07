0811590837c21bef99823032d100d3c5b558bfed
phanan1608 <96655589+phanan1608@users.noreply.github.com>
2022-07-07 20:17:27 +0700

    AnP update FE code 07/07

--- sprint1/src/app/app.component.ts
+++ sprint1/src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
         this.isGuest = false;
       }
     }
-    // this.isGuest = false;
+    this.isGuest = false;
   }
 
   ngAfterViewChecked(): void {
