8fd62aab013e63dd8974dfc677781a38cfb1a377
trangiang911 <tbgiang191199@gmail.com>
2022-07-08 09:35:33 +0700

    GiangTB up git

--- sprint1/src/app/component/customer/customer-create/customer-create.component.ts
+++ sprint1/src/app/component/customer/customer-create/customer-create.component.ts
@@ -33,7 +33,7 @@ export class CustomerCreateComponent implements OnInit {
     this.createForm = new FormGroup({
       customerId: new FormControl(''),
       customerName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20),
-        Validators.pattern('^[A-Za-zÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠ-ỹ][\\s\\S]*$')]),
+        Validators.pattern('^([a-zA-ZxzÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠyỳọầảấờễạằệếộậốứữịỗềểẩớặồợụủỹắẫựỉỏừỷởửỵẳẹẽổẵẻỡ]+)((\\s{1}[a-zA-ZxzÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠyỳọầảấờễạằệếộậốứữịỗềểẩớặồợụủỹắẫựỉỏừỷởửỵẳẹẽổẵẻỡ]+){1,})$')]),
       customerBirthday: new FormControl('', [Validators.required]),
       customerGender: new FormControl('', [Validators.required]),
       customerAddress: new FormControl(''),
@@ -62,14 +62,14 @@ export class CustomerCreateComponent implements OnInit {
 
     this.customerService.create(customer).subscribe(() => {
       }, error => {
-        this.toastr.warning('Bắt buộc phải nhập đúng thông tin !', '', {
+        this.toastr.warning('Bắt buộc phải nhập đúng thông tin !', 'Thông báo', {
           timeOut: 3000,
           progressBar: true
         });
 
       },
       () => {
-        this.toastr.success('Thêm mới thành công !', '', {
+        this.toastr.success('Thêm mới thành công !', 'Thông báo', {
           timeOut: 3000,
           progressBar: true
         });
