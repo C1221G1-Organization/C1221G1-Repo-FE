64c26d3b2103e9a51a5d80338e3b8a906137c41c
nghiatrinh107 <nghiatrinh107@gmail.com>
2022-07-13 16:14:31 +0700

    TrinhNN

--- sprint1/src/app/dto/medicine-of-invoice-dto.ts
+++ sprint1/src/app/dto/medicine-of-invoice-dto.ts
@@ -2,6 +2,11 @@ import {Medicine} from "../model/medicine/medicine";
 
 
 export interface MedicineOfInvoiceDto {
-  medicine: Medicine
-  invoiceMedicineQuantity: number
+  medicine: Medicine,
+  quantityRefund?: number,
+  medicineUnit?: string,
+  price?: number,
+  medicineName?: string,
+  invoiceMedicineQuantity?: number,
+  moneyOfMedicine?: number
 }
